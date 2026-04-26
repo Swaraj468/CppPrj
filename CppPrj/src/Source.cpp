e0524ed18245f91cf4659a96938a712f65387e6a
Swaraj <swarajbandhi0000@gmail.com>
2026-04-26 16:54:28 +0530

    done with constructors

--- CppPrj/src/Source.cpp
+++ CppPrj/src/Source.cpp
@@ -7,6 +7,19 @@
 class Student
 {
 public:
+	Student(const std::string& studentName, int studentID, int studentYear)//a consturctor is a function that has no return type, and has the same name as the class
+
+		: name{ studentName }, ID{ studentID }, year{ studentYear }	// initializer more performant than doing the assignement in the body of the constructor
+	{
+		// this is a default constructor. if you do not provide any constructor, this will be automatically created. if you do provide a constructor, then default constructor will not be created for you
+
+		std::cout << "custom constructor called" << std::endl;
+	}
+	Student(const char* rawStringName, int  studentID, int studentYear) //you can create different constructors. as long as their signature are different.
+		:Student{ std::string{rawStringName}, studentID, studentYear }//: name{rawStringName}, ID{studentID}, year{studentYear}
+	{
+		std::cout << "raw char constructor called" << std::endl;
+	}
 	std::string name;
 	int ID;
 	int year;
@@ -16,16 +29,9 @@ private:
 
 
 int main() {
-	Student studentOne; //making an instance of student. a real variable.
-	studentOne.name = "Sam"; //assigning the value for studentOne's member.
-	studentOne.ID = 12111470;
-	studentOne.year = 1;
+	Student studentOne{"Sam", 12111470, 1}; //making an instance of student. a real variable.
 
-	Student studentTwo;
-	studentTwo.name = "Emily";
-	studentTwo.ID = 002;
-	studentTwo.year = 2;
-
+	Student studentTwo{"Emily", 002, 2};
 	std::vector<Student> students = { studentOne, studentTwo };
 	for (Student& student : students) {
 		std::cout << "Student name: " << student.name << std::endl;
