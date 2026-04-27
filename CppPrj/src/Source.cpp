89598d3ac3f12977f1aaccfb92aa6d1bce88cb63
Swaraj <swarajbandhi0000@gmail.com>
2026-04-28 02:24:13 +0530

    done till overriding and virtual function but I need to explore more

--- CppPrj/src/Source.cpp
+++ CppPrj/src/Source.cpp
@@ -4,139 +4,64 @@
 #include "Header.h"
 #include <memory>
 
-
-
-//defineing a custom type (class), what it has.
-class Student
+class Vehicle
 {
 public:
-	Student(const std::string& studentName, int studentID, int studentYear)//a consturctor is a function that has no return type, and has the same name as the class
-
-		: name{ studentName }, ID{ studentID }, year{ studentYear }	// initializer more performant than doing the assignement in the body of the constructor
+	Vehicle(const std::string& vehicleName, const int vehicleCapacity)
+		: _name{vehicleName}, _capacity{vehicleCapacity}
 	{
-		std::cout << "custom constructor called" << std::endl;
+
 	}
-		Student(const char* rawStringName, int  studentID, int studentYear) //you can create different constructors. as long as their signature are different.
-		:Student{ std::string{rawStringName}, studentID, studentYear }//: name{rawStringName}, ID{studentID}, year{studentYear}
+	 virtual void Run() const //virtual means the child can override it
 	{
-		std::cout << "raw char constructor called" << std::endl;
-	}
-	~Student() {
-		std::cout << "student " << name << " is destructed" << std::endl;
-	}
-	void sayHello() {
-		std::cout << "Say Hi " << name << std::endl;
-	}
+		std::cout << _name << " with " << _capacity << " people running" << std::endl;
+	} 
 
-	void IncrementYear() {
-		year++;
-		switch (year)
-		{
-		case 1:
-			std::cout<< name<< " is in freshman"<<std::endl;
-			break;
-		case 2:
-			std::cout << name << " is in 2nd year" << std::endl;
-			break;
-		case 3:
-			std::cout << name << " is in junior year" << std::endl;
-			break;
-		case 4:
-			std::cout << name << " is in last year" << std::endl;
-			break;
-
-		default:	
-			std::cout << name << " is graduated" << std::endl;
-			break;
-		}
-	}
-	bool IsGraduated() {
-		return year > 4;
-	}
-	//setters
-	void setId(int newID) {
-		if (newID < 0) 
-		{
-			std::cout << "Wrong Id Value, it should be positive";
-			return;
-		}
-		ID = newID;
-	}
-	//getters
-	int GetId() {
-		return ID;
-	}
-
-	std::string name;
-	
-
+	 int GetCapacity() const { return _capacity; }
+	 void SetCapacity(int newCapacity) {
+		 if (newCapacity < 0) {
+			 return;
+		 }
+		 _capacity = newCapacity;
+	 }
+protected: // weak protection, the child class can access it, still not available out side of the family, use with caution.
+	const std::string _name; //and _ infromnt of a member indicates private. in python _ is the way to tell a member is private.
 
 private:
-	int ID;
-	int year; //to have the member in the private section. and provide the private function to alter the value.
-	int secrate;
+	
+	 int _capacity;
+
+};
+
+class Car : public Vehicle //car is inheriting from vehicle
+{
+public:
+	Car(const std::string& carName, const int carCapacity, float carMileage)
+		: Vehicle{ carName, carCapacity }, _mileage{ carMileage }
+	{
+
+	}
+	virtual void Run() const override //override means we are not creating a new function. we are redefining the vehicle part of the run for the car
+	{
+		std::cout << _name << " with " << _mileage << " mileage " << GetCapacity() << " people running" << std::endl;
+	}
+	 float GetMileage() const { //accesser.
+		return _mileage;
+	}
+private:
+	float _mileage;
 };
 
 int main() {
-	Student studentOne{ "Sam", 12111470, 1 }; //making an instance of student. a real variable.
-	studentOne.sayHello();
-	while (!studentOne.IsGraduated()) {
-		studentOne.IncrementYear();
-	}
-	studentOne.setId(1);
-	std::cout << studentOne.GetId() << std::endl;
+	Vehicle vehicle{ "LightYear2000", 1000 };
+	//vehicle.Run();
 
+	Car car{ "Toyota Rv4", 5 , 20.5};
+	Vehicle& carAsVehicle = car;
+
+
+	car.Run();
+	carAsVehicle.Run();
+	//std::cout << car.GetMileage() << std::endl;
 }
 
-
-//class Car {
-//public:
-//	std::string carName;
-//	
-//	std::string carType;
-//	Car(const std::string& Name, int Number, const std::string& Type, const std::string& Gas)
-//		: carName{ Name }, RegNumber{ Number }, carType{ Type }, TotalGas{Gas}
-//	{
-//		std::cout << "Constructor is created" << std::endl;
-//	}
-//
-//	~Car() {
-//		std::cout << "Constructor is dead" << std::endl;
-//	}
-//	void setRegNumber(int num) {
-//		if (num < 0) {
-//			std::cout << "The car number is wrong";
-//			return;
-//		}
-//		RegNumber = num;
-//	}
-//	int GetRegNumber() {
-//		return RegNumber;
-//	}
-//
-//	void setTotaGas(std::string haas) {
-//		if (haas.empty() && std::all_of(haas.begin(), haas.end(), ::isdigit)) {
-//			std::cout << "Wrong value for the game" << std::endl;
-//			return;
-//		}
-//		TotalGas = haas;
-//	}
-//	std::string GetTotalGas() {
-//		return TotalGas;
-//	}
-//private:
-//	int RegNumber;
-//	std::string TotalGas;
-//};
-//
-//int main() {
-//	Car carOne{ "Toyota Fortuner", 5634, "SUV", "65 liters"};
-//	carOne.setRegNumber(-1);
-//	std::cout << carOne.GetRegNumber() << std::endl;
-//
-//	carOne.setTotaGas("");
-//	std::cout << carOne.GetTotalGas() << std::endl;
-//
-//
-//}
-//
